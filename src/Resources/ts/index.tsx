250eae32c4e11466cd21cf25ce415902636c1016
phuwanat.r <ggamegg170@gmail.com>
2024-06-27 23:28:38 +0700

    Remose suspense

--- src/Resources/ts/index.tsx
+++ src/Resources/ts/index.tsx
@@ -1,25 +1,16 @@
 import "../css/index.css";
-import React from "react";
 import { createRoot } from "react-dom/client";
 import Layout from "./components/layout/Layout";
 
 const page = (window as any)._react_page;
 const props = (window as any)._react_props;
 
-const Page = React.lazy(() => import(`./Pages/${page}`));
-
-let Loader = undefined;
-try {
-  Loader = require(`./Pages/${page}.loading`)?.default;
-} catch {}
-
 const root = createRoot(document.getElementById("app") as HTMLElement);
-root.render(
-  <Layout>
-    <React.Suspense fallback={Loader ? <Loader /> : <></>}>
-      <Page {...props} />
-    </React.Suspense>
-  </Layout>
-);
-
-document.getElementById("react-value")?.remove();
+import(`./Pages/${page}`).then((Page) => {
+  root.render(
+    <Layout>
+      <Page.default {...props} />
+    </Layout>,
+  );
+  document.getElementById("react-value")?.remove();
+});
